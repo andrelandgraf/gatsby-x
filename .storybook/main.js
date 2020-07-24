4baffc9242a0f9a05fbca9009162d0902b762552
Andre Landgraf <andre.timo.landgraf@gmail.com>
2020-07-23 22:17:03 -0700

    comments out svg fix to debug deployment

--- .storybook/main.js
+++ .storybook/main.js
@@ -24,16 +24,16 @@ module.exports = {
     // Add SVGR Loader
     // ========================================================
     // Remove svg rules from existing webpack rule
-    const fileLoaderRule = config.module.rules.find(
-      (rule) => rule.test && rule.test.test(".svg")
-    );
-    fileLoaderRule.exclude = /\.svg$/;
+    // const fileLoaderRule = config.module.rules.find(
+    //   (rule) => rule.test && rule.test.test(".svg")
+    // );
+    // fileLoaderRule.exclude = /\.svg$/;
 
-    config.module.rules.push({
-      test: /\.svg$/,
-      enforce: "pre",
-      loader: require.resolve("svg-react-loader")
-    });
+    // config.module.rules.push({
+    //   test: /\.svg$/,
+    //   enforce: "pre",
+    //   loader: require.resolve("svg-react-loader")
+    // });
 
     return config
   },
